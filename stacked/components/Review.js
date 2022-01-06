1889fb650a377938cd46ecf250fa3f9f02b33e4d
Lo-pez <diego.lopez13@upr.edu>
2022-01-05 22:08:43 -0400

    Added Home, Review, UserData components

---
+++ stacked/components/Review.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Review = () => {
+    return (
+        <View>
+            <Text>Review</Text>
+        </View>
+    )
+}
+
+export default Review
+
+const styles = StyleSheet.create({})
---
+++ stacked/components/UserData.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const UserData = () => {
+    return (
+        <View>
+            <Text>Userdata</Text>
+        </View>
+    )
+}
+
+export default UserData
+
+const styles = StyleSheet.create({})
