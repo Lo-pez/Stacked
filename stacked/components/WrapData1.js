1f62358edfe4204e934658719f66e0fbc97fe12a
James <jameshan20024@gmail.com>
2022-01-06 11:55:23 -0600

    WrapData1 Created, Routed Wrapped and WrapData1.
    If you want to navigate it, use the navigation.navigate() on SplashScreen and change that into "wrapped".

---
+++ stacked/components/WrapData1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+const WrapData1 = ({navigation}) => {
+  return (
+    <View>
+      <Text>Hellloooo</Text>
+    </View>
+  );
+};
+
+export default WrapData1;
+
+const styles = StyleSheet.create({});
